baf81c455457479fcfba7af0a91ad12514fdd2fb
i.cappelli <i.cappelli@cineca.it>
2022-07-21 12:37:15 +0200

    secondo commit

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ReactiveFormsModule} from "@angular/forms";
 import {RecipesComponent} from "./recipes.component";
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
 import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
@@ -10,7 +9,6 @@ import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
 import {RecipesRouteModule} from "./recipes-route.module";
 import {SharedModule} from "../shared/shared.module";
-import {DirectivesModule} from "../shared/directives.module";
 
 
 @NgModule({
@@ -27,9 +25,6 @@ import {DirectivesModule} from "../shared/directives.module";
     ReactiveFormsModule,
     RecipesRouteModule,
     SharedModule,
-    //DirectivesModule,
-    //CommonModule,
-
   ]
 })
 
